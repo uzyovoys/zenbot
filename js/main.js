5d7d4d792ff6f32ed084b493b4f0f0eccb3922eb
Andre Polykanine A.K.A. Menelion Elensúlë <andre@oire.org>
2016-06-09 14:35:34 +0300

    Tiny fix to main.js: it is recommended to use on() instead of event functions in jQuery

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * Created by morfeusys on 11.05.16.
  */
 (function($) {
-    $("#menu-toggle").click(function(e) {
+    $("#menu-toggle").on("click", function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
